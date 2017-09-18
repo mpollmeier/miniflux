804d45d733c7938d9e41ce6c54a7a908bf72fe98
Michael Pollmeier <michael@michaelpollmeier.com>
2017-09-18 20:56:19 +1200

    start apache in background, tail error log as foreground process instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,6 @@ service cron start
 
 mkdir -p /var/www/app/data/favicons
 chown -R www-data:www-data /var/www/app/data
-/usr/sbin/apache2ctl -D FOREGROUND 
+#/usr/sbin/apache2ctl -D FOREGROUND 
+service apache2 start
+tail -f /var/log/apache2/error.log
